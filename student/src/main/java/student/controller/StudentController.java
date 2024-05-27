6a7200ff325362058da8ccbcff3af76bb272c911
AhmedSamir <ahmed.samir@eden-tech.io>
2024-05-27 10:53:01 +0300

    [add]

--- student/src/main/java/student/controller/StudentController.java
+++ student/src/main/java/student/controller/StudentController.java
@@ -5,13 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import student.model.dto.StudentDto;
 import student.service.StudentService;
 
+import java.util.List;
+
 import java.util.HashMap;
+import java.util.Optional;
 
 @RestController
 public class StudentController {
@@ -29,5 +30,20 @@ public class StudentController {
             return ResponseEntity.badRequest().body(errors);
         }
         return ResponseEntity.ok(studentService.addStudent(studentDto));
+    }
+
+    @GetMapping("/students")
+    public List<StudentDto> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+    @GetMapping("/student/{id}")
+    public Optional<StudentDto> getStudentById(@PathVariable Long id) {
+        return studentService.getStudentById(id);
+    }
+
+    @PutMapping("/student/{id}")
+    public Optional<StudentDto> updateStudent(@Valid @RequestBody StudentDto studentDto, @PathVariable Long id) {
+        return studentService.updateStudent(studentDto, id);
     }
 }
--- student/src/main/java/student/service/StudentService.java
+++ student/src/main/java/student/service/StudentService.java
@@ -1,11 +1,18 @@
 package student.service;
 
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.mapstruct.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 import student.mapper.StudentMapper;
 import student.model.dto.StudentDto;
 import student.repository.StudentRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class StudentService {
@@ -15,6 +22,27 @@ public class StudentService {
     @Transactional
     public StudentDto addStudent(StudentDto sudentDto) {
         return StudentMapper.INSTANCE.studentToDto(studentRepository.save(StudentMapper.INSTANCE.dtoToStudent(sudentDto)));
+    }
+
+    @Transactional
+    public List<StudentDto> getAllStudents() {
+        return studentRepository.findAll().stream().map(student -> StudentMapper.INSTANCE.studentToDto(student)).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public Optional<StudentDto> getStudentById(Long id) {
+        return studentRepository.findById(id).map(student -> StudentMapper.INSTANCE.studentToDto(student));
+    }
+
+    @Transactional
+    public Optional<StudentDto> updateStudent(@Valid @RequestBody StudentDto studentDto, Long id) {
+        Optional<StudentDto> optionalStudentDto = studentRepository.findById(id).map(student -> StudentMapper.INSTANCE.studentToDto(student));
+        if (optionalStudentDto.isPresent()) {
+            optionalStudentDto.get().setName(studentDto.getName());
+            optionalStudentDto.get().setSchoolId(studentDto.getSchoolId());
+            studentRepository.save(StudentMapper.INSTANCE.dtoToStudent(optionalStudentDto.get()));
+        }
+        return optionalStudentDto;
     }
 
 }
